92a51426e60c9cb8b654fafde29f67eb27e97aa3
Rachel Wong <rwong1@andrew.cmu.edu>
2017-02-06 00:19:19 -0500

    byte 2 edits

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+import json
+import urllib
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -5,14 +20,18 @@ app.config['DEBUG'] = True
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+    
+API_KEY = AIzaSyBFJIMR8IQl2UIUT07gwrfDu4Ifnn1txEA
+
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+service = build('fusiontables', 'v1', developerKey=API_KEY)
